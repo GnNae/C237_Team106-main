2c4e30bb271f1eb8eb9ca182795d7350f53d4e55
LimKaiXin <24002707@myrp.edu.sg>
2025-07-29 22:51:57 +0800

    Update app.js
    added 
    app.get('/petGuide', (req, res) => {
        res.render('petguide', { user: req.session.user });
    });

--- app.js
+++ app.js
@@ -405,5 +405,10 @@ app.get('/feedbackS', (req, res)=>{
     });
 });
 
+// petguide
+app.get('/petGuide', (req, res) => {
+    res.render('petguide', { user: req.session.user });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
